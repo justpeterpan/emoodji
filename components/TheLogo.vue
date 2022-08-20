3faa1ed0ad2884634c362969d0a6f27fd25a2a6d
MogliCodes <moglicodes@gmail.com>
2022-08-20 11:44:34 +0200

    add layout, pages and font

---
+++ components/TheLogo.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class='fixed w-full text-center py-2'>
+    <p class='font-bold'>emoodji</p>
+  </header>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class='min-h-screen flex items-center'>
+    <div>
+      <h1 class='text-4xl font-bold mb-16'>Start tracking your mood</h1>
+      <div class='mb-4'>
+        <label class='block' for=''>Mail</label>
+        <input class='block bg-gray-600 text-white rounded shadow-brutal-sm w-full p-2' type='text'>
+      </div>
+      <div class='mb-8'>
+        <label class='block' for=''>Password</label>
+        <input class='block bg-gray-600 text-white rounded shadow-brutal-sm w-full p-2' type='text'>
+      </div>
+      <div>
+        <button class='bg-green-500 block font-bold rounded shadow-brutal-sm w-full py-2'>Login</button>
+      </div>
+    </div>
+  </section>
+</template>

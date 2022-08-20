e0f23c41410eb9beb96a48e29d649f14b95aad4e
MogliCodes <moglicodes@gmail.com>
2022-08-20 12:03:12 +0200

    feat: add layout, pages and font

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,14 @@
     <slot></slot>
   </main>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class='min-h-screen flex items-center'>
+  <section class='min-h-screen flex items-center justify-center'>
     <div>
       <h1 class='text-4xl font-bold mb-16'>Start tracking your mood</h1>
       <div class='mb-4'>
@@ -11,8 +11,16 @@
         <input class='block bg-gray-600 text-white rounded shadow-brutal-sm w-full p-2' type='text'>
       </div>
       <div>
-        <button class='bg-green-500 block font-bold rounded shadow-brutal-sm w-full py-2'>Login</button>
+        <button class='bg-green-500 block font-bold rounded shadow-brutal-sm w-full py-2' @click="login" >Login</button>
       </div>
     </div>
   </section>
 </template>
+
+<script setup lang='ts'>
+const router = useRouter();
+function login(){
+  router.push('/')
+}
+
+</script>

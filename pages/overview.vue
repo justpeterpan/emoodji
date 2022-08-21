f173f7f9e4b128874b46b2af4f0eed2e9c4a67c4
MogliCodes <moglicodes@gmail.com>
2022-08-21 13:21:37 +0200

    resolve merge conflicts

--- pages/overview.vue
+++ pages/overview.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="py-24">
-    <div class="max-w-sm mx-auto">
-      <h1 class="font-bold text-2xl mb-8 text-center">Overview</h1>
+    <div class='max-w-sm mx-auto'>
+      <h1 class='font-bold text-2xl mb-8 text-center'>Overview</h1>
     </div>
   </section>
 </template>

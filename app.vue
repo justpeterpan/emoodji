a565a3adef84ccd72e72ef482d2b4d991765ab3c
MogliCodes <moglicodes@gmail.com>
2022-08-21 19:11:37 +0200

    add login hint and show nav only when logged in

--- app.vue
+++ app.vue
@@ -3,14 +3,5 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-    <TheNavigation />
   </div>
 </template>
-
-
-<script>
-import TheNavigation from './components/AppLayout/TheNavigation'
-export default {
-  components: { TheNavigation }
-}
-</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="min-h-screen flex items-center justify-center">
+  <section v-if='!requestedLogin' class="min-h-screen flex items-center justify-center">
     <div>
       <h1 class="text-4xl font-bold mb-16">Start tracking your mood</h1>
       <div class="mb-4">
@@ -7,11 +7,14 @@
         <input v-model="mail" class="block bg-gray-600 text-white rounded shadow-brutal-sm w-full p-2" type="text" />
       </div>
       <div>
-        <button class="bg-green-500 block font-bold rounded shadow-brutal-sm w-full py-2" @click="auth.signIn({ email: mail })">
+        <button class="bg-green-500 block font-bold rounded shadow-brutal-sm w-full py-2" @click="loginUser()">
           Login
         </button>
       </div>
     </div>
+  </section>
+  <section v-else class="min-h-screen flex items-center justify-center">
+    <p class='text-center'>We sent you a magic link to log in to emoodji. Just check your mails.</p>
   </section>
 </template>
 
@@ -21,9 +24,16 @@ const user = useSupabaseUser()
 const { auth } = useSupabaseClient()
 const mail = ref('')
 
+const requestedLogin = ref(false)
+
 watchEffect(() => {
   if (user.value) {
     navigateTo('/')
   }
 })
+
+function loginUser(){
+  auth.signIn({ email: mail.value })
+  requestedLogin.value = true
+}
 </script>
